6f24cdf0a732152bf74fa6fd965cb12905434407
japon27 <j4pon@hotmail.com>
2019-05-15 01:13:33 -0500

    probando en java

---
+++ jboss/jbr/src/main/java/pe/ripley/jbr/App.java
@@ -0,0 +1,30 @@
+package pe.ripley.jbr;
+
+import static spark.Spark.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	public static void main(String[] args) {
+
+		staticFiles.location("/public");
+
+		get("/hello", (request, response) -> {
+			Map<String, Object> model = new HashMap<>();
+			model.put("message", "Hello Velocity");
+			return new ModelAndView(model, "hello.vm"); // located in the
+														// resources directory
+		}, new VelocityTemplateEngine());
+
+		init();
+
+	}
+}
